f4a8b3857c0951ac818d8c7a1fe483a0153c8aac
erik-nathan <eriknathan.contato@gmail.com>
2023-04-16 18:59:05 -0300

    :boom: fix: atualizações na Lib login

--- lib/login/acess.py
+++ lib/login/acess.py
@@ -1,6 +1,8 @@
-from lib.interface import cabecalho
+from lib.interface import cabecalho, linha
 from getpass import getpass
+from lib.sistema import sistema
 import time
+
 
 usuario_correto = 'admin'
 senha_correta = 'admin'
@@ -11,9 +13,10 @@ def fazerLogin():
     login = str(input('- Login: '))
     password = getpass('- Senha: ')
     if login == usuario_correto and password == senha_correta:
+        linha()
         print('Acessando sistema...')
         time.sleep(2)
-        print('AQUI CHAMA O SISTEMA!!!!')
+        sistema()
     else:
         print('Acesso Negado!')
         return fazerLogin()
