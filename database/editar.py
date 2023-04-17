178f5c29d68dbfca2c70f528a0e91812a960b074
erik-nathan <eriknathan.contato@gmail.com>
2023-04-17 09:13:51 -0300

    :sparkles: feat: Adicionando novas funções em Editar

--- database/editar.py
+++ database/editar.py
@@ -9,3 +9,23 @@ def editar(id, nome, idade):
 
     con.commit()
     con.close()
+
+
+def editar_nome(id, nome):
+    con = sqlite3.connect('pessoas.db')
+    cur = con.cursor()
+
+    cur.execute("UPDATE pessoas SET nome = ? WHERE id = ?", (nome, id))
+
+    con.commit()
+    con.close()
+
+
+def editar_idade(id, idade):
+    con = sqlite3.connect('pessoas.db')
+    cur = con.cursor()
+
+    cur.execute("UPDATE pessoas SET idade = ? WHERE id = ?", (idade, id))
+
+    con.commit()
+    con.close()
--- lib/sistema/editar_pessoa.py
+++ lib/sistema/editar_pessoa.py
@@ -1,10 +1,38 @@
+import time
+
 from lib.interface import cabecalho
-from database.editar import editar
+from database.editar import editar, editar_nome, editar_idade
+from lib.interface.menu import menu
 
 
 def editar_pessoa():
     cabecalho("EDITAR PESSOA")
     id = int(input("Digite o ID da pessoa que deseja editar: "))
-    nome = input("Digite o novo nome da pessoa: ")
-    idade = int(input("Digite a nova idade da pessoa: "))
-    editar(id,nome, idade)
+
+    print('Você deseja editar: ')
+    opcao = menu([
+        'Nome',
+        'Idade',
+        'Ambos'])
+
+    if opcao == 1:
+        nome = str(input('Digite o nome nome: '))
+        editar_nome(id, nome)
+        time.sleep(1)
+        print('Nome atualizado com sucesso! ')
+        time.sleep(2)
+    elif opcao == 2:
+        idade = input('Digite a nova idade: ')
+        editar_idade(id, idade)
+        time.sleep(1)
+        print('Idade atualizada com sucesso! ')
+        time.sleep(2)
+    elif opcao == 3:
+        nome = input("Digite o novo nome da pessoa: ")
+        idade = int(input("Digite a nova idade da pessoa: "))
+        editar(id,nome, idade)
+        time.sleep(1)
+        print('Pessoa atualizada com sucesso! ')
+        time.sleep(2)
+    else:
+        print('\033[31mERRO! Digite uma opção válida!\033[m')
